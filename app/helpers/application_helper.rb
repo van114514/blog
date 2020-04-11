6ef6fdfa19c5b69b57a19d2d92ed0803b47ba707
van114514 <van45450721919@gmail.com>
2020-04-11 20:09:57 +0900

    Set RSpec

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,7 @@
 module ApplicationHelper
 
     def full_title(page_title = '')
-      base_title = "ばんしこブログ"
+      base_title = "Van Blog"
       if page_title.empty?
         base_title
       else
